dafb37e143c62e97394e4ed2c5a427d6d1bc34e9
bubus128 <72924140+bubus128@users.noreply.github.com>
2021-10-25 07:26:19 +0200

    database creating script

---
+++ sql scripts/database_create.sql
@@ -0,0 +1,28 @@
+drop database if exists ships;
+create database ships;
+
+use ships;
+
+create table UserRole(
+	id int primary key NOT NULL auto_increment,
+    role_name VARCHAR(45) NOT NULL
+);
+
+create table Users(
+	id int primary key not null auto_increment,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    pass VARCHAR(45) NOT NULL,
+    username VARCHAR(45) NOT NULL,
+    user_role int not null,
+    foreign key(user_role) references UserRole(id)
+);
+
+create table Application(
+	id int primary key not null auto_increment,
+    applicant int not null,
+    clerk int not null,
+    foreign key(applicant) references Users(id),
+    foreign key(clerk) references Users(id)
+    # TODO: fields from form
+);
