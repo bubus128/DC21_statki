b03149924435fba407ebafcd025a875eafc2d17f
Michał Wnuczyński <49735204+NukeeMann@users.noreply.github.com>
2021-11-15 23:13:24 +0100

    Dodanie wszystkich formularzy

--- petitioner_app/views.py
+++ petitioner_app/views.py
@@ -1,6 +1,6 @@
 from mailmerge import MailMerge
 from django.shortcuts import render
-from .forms import VehRegisterForm
+from .forms import VehRegisterForm, VehDeregisterForm, VehReregisterForm
 from django.template import loader
 from django.http import HttpResponse
 from petitioner_app.models import Application
@@ -21,10 +21,9 @@ def vehregister(request):
     if request.method == 'POST':
         form = VehRegisterForm(request.POST or None)
         if form.is_valid():
-            form.save()
             form_dict = form.data.dict()
-            template = "petitioner_app\\templates\\converter\\formTemplate.docx"
-            output_document = "output.docx"
+            template = "petitioner_app\\templates\\converter\\form2Template.docx"
+            output_document = "output1.docx"
             libre_office_path = 'C:\\Program Files\\LibreOffice\\program\\soffice'
             # Create docx file due to template
             document = MailMerge(template)
@@ -37,19 +36,68 @@ def vehregister(request):
             # Create an application
             application = Application(petitioner=request.user, form=form.instance)
             application.save()
+            form.save()
 
     context = {'form': form}
     return render(request, "petitioner_app/vehregister.html", context)
 
 
-def form2(request):
-    template = loader.get_template('petitioner_app/form2.html')
-    return HttpResponse(template.render({}, request))
+def vehderegister(request):
+    form = VehDeregisterForm(request.POST or None)
+
+    if request.method == 'POST':
+        form = VehDeregisterForm(request.POST or None)
+        if form.is_valid():
+            form_dict = form.data.dict()
+            form_dict['rodzaj_pojazdu'] = form_dict.pop('vehicle_id')
+            form_dict['rok_produkcji'] = form_dict.pop('reason')
+            template = "petitioner_app\\templates\\converter\\form2Template.docx"
+            output_document = "output2.docx"
+            libre_office_path = 'C:\\Program Files\\LibreOffice\\program\\soffice'
+            # Create docx file due to template
+            document = MailMerge(template)
+            document.merge_pages([form_dict])
+            document.write(output_document)
+            document.close()
+            # Convert docx to pdf (using libreoffice)
+            args = [libre_office_path, '--headless', '--convert-to', 'pdf', output_document]
+            subprocess.run(args)
+            # Create an application
+            application = Application(petitioner=request.user, form=form.instance)
+            application.save()
+            form.save()
+
+    context = {'form': form}
+    return render(request, "petitioner_app/vehderegister.html", context)
 
 
-def form3(request):
-    template = loader.get_template('petitioner_app/form3.html')
-    return HttpResponse(template.render({}, request))
+def vehreregister(request):
+    form = VehReregisterForm(request.POST or None)
+
+    if request.method == 'POST':
+        form = VehReregisterForm(request.POST or None)
+        if form.is_valid():
+            form_dict = form.data.dict()
+            form_dict['rodzaj_pojazdu'] = form_dict.pop('current_owner_id')
+            form_dict['rok_produkcji'] = form_dict.pop('new_owner_id')
+            template = "petitioner_app\\templates\\converter\\form3Template.docx"
+            output_document = "output3.docx"
+            libre_office_path = 'C:\\Program Files\\LibreOffice\\program\\soffice'
+            # Create docx file due to template
+            document = MailMerge(template)
+            document.merge_pages([form_dict])
+            document.write(output_document)
+            document.close()
+            # Convert docx to pdf (using libreoffice)
+            args = [libre_office_path, '--headless', '--convert-to', 'pdf', output_document]
+            subprocess.run(args)
+            # Create an application
+            application = Application(petitioner=request.user, form=form.instance)
+            application.save()
+            form.save()
+
+    context = {'form': form}
+    return render(request, "petitioner_app/vehreregister.html", context)
 
 
 def myforms(request):
