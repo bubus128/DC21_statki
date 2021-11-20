c718aa19c025605a963d27f96f8b5db320f36dc2
Szymon Trzebiatowski <szym.trzebiatowski@gmail.com>
2021-11-20 14:18:47 +0100

    /myissues now shows existing assigned issues

--- clerk_app/views.py
+++ clerk_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from petitioner_app.models import VehRegister, VehDeregister, VehReregister, Application
 
 
 # Create your views here.
@@ -10,5 +10,27 @@ def landing(request):
 
 
 def myissues(request):
+
+    current_user_id = request.user.id
+    my_issues = Application.objects.filter(clerk=current_user_id)
     template = loader.get_template('clerk_app/my_issues.html')
-    return HttpResponse(template.render({}, request))
+
+    vehregister_applications = []
+    vehderegister_applications = []
+    vehreregister_applications = []
+
+    for issue in my_issues:
+        if issue.vehregister_form is not None:
+            vehregister_applications.append(issue)
+        elif issue.vehderegister_form is not None:
+            vehderegister_applications.append(issue)
+        elif issue.vehreregister_form is not None:
+            vehreregister_applications.append(issue)
+
+    context = {
+        'vehregister_applications': vehregister_applications,
+        'vehderegister_applications': vehderegister_applications,
+        'vehreregister_applications': vehreregister_applications,
+    }
+
+    return HttpResponse(template.render(context, request))
